766a9253f36f0f4ecc7d0119ddb694ee6195756f
1container <85973459+1container@users.noreply.github.com>
2021-09-20 16:01:27 +0800

    Add files via upload

---
+++ 程序2 不同初始化中心对分类性能的影响.py
@@ -0,0 +1,98 @@
+import math
+import pandas as pd 
+import numpy as np 
+# 读取数据
+import csv
+with open('F:\\watermelon.csv','r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+# 数据处理
+file = np.array(rows); file1 = file[1:,1:]#去除第一列第一行
+file2=pd.DataFrame(file1)# 去除重复数据
+file2.drop_duplicates(inplace=True)
+file3=np.array(file2); data = file3.astype(float)# 转换格式
+# 计算两向量的欧氏距离
+def distEclud(vecA, vecB):
+    return np.sqrt(np.sum(np.square(vecA-vecB)))
+
+# 循环
+def kMeans(data,matmean,dist=distEclud):
+    le=len(data)
+    cl = np.mat(np.zeros((le,2)))#创建结果分类矩阵
+    for q in range(999):
+        for i in range(le):
+            mindist=99999;index=-1
+        # 计算样本与均值向量的距离，确定样本簇标记
+            for j in range(2):
+                distji = dist(matmean[j],data[i])
+                if distji < mindist:
+                    mindist = distji;index = j
+            cl[i,:]=index,mindist
+    # 计算新均值向量
+        mattest = np.mat(np.zeros((2,2)))
+        for p in range(2):
+            clust = data[np.where(cl[:,0]==p)[0]]
+            mattest[p,:] = np.mean(clust,axis=0)#若将mattest初始化放于q的for循环外，此处第二次循环matmean随mattest变化
+        if (mattest == matmean).all() == True:#均值向量未改变
+            break
+        else:
+            matmean=mattest
+        #迭代第q次结果与q-1次相同
+    sumcl=np.sum(cl,axis=0)
+    sse=sumcl[0,1]
+    return [matmean,cl,sse,q-1]
+
+# 计算簇间距离
+def distbet(matmean, data):
+    allmean=np.mean(data,axis=0)
+    mean=np.tile(allmean,(2,1))
+    return np.sum(np.sqrt(np.sum(np.square(matmean-mean),axis=1)))/2#行相加开根号再相加
+# 计算簇内距离
+def distin(matmean, cl):
+    num0=np.sum(cl[:,0]==0)
+    num1=np.sum(cl[:,0]==1)
+    dist0=np.sum(cl[np.where(cl[:,0]==0)[0],1])
+    dist1=np.sum(cl[np.where(cl[:,0]==1)[0],1])
+    return dist0/num0 + dist1/num1
+
+# 穷举随机组合 得到相应度量指标
+ass=[]
+for i in range(30):
+    for j in range(i+1,30):
+        randvec=np.mat(np.zeros((2,2)))
+        randvec=np.vstack((data[i],data[j]))
+        a=kMeans(data,randvec)
+        matmean=a[0]
+        cl=a[1]
+        x=distbet(matmean,data)
+        y=distin(matmean,cl)
+        disass=distbet(matmean,data)/distin(matmean,cl)
+        ass.append(disass)#评价指标形成列表
+print(max(ass),min(ass))
+
+# 指标散点图
+x=range(len(ass))
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']#图片显示中文，参考网址：https://blog.csdn.net/seveneleve/article/details/108974201
+p1=plt.scatter(x,ass,marker='.')
+plt.title('西瓜数据集穷举初始化中心的分类性能指数散点图')
+plt.xlabel("编号")
+plt.ylabel("指数")
+plt.show()
+
+# #可视化聚类结果
+# import matplotlib.pyplot as plt
+# for i in range(le):
+#     if cl[i,0]==0:
+#         plt.plot(data[i,0],data[i,1],'sg')
+#     elif cl[i,0]==1:
+#         plt.plot(data[i,0],data[i,1],'or')
+# plt.plot(matmean[0,0],matmean[0,1],'.k')
+# plt.plot(matmean[1,0],matmean[1,1],'.k')
+# plt.title('Classification of watermelons')
+# plt.xlabel('density')
+# plt.ylabel('sugercontent')
+# plt.grid(c='silver',linestyle='--')
+# plt.xlim([0.2,0.9])  # x轴边界
+# plt.ylim([0,0.8])  # y轴边界
+# plt.show()
---
+++ 程序3k-means++ 初始化中心方法改进.py
@@ -0,0 +1,96 @@
+import math
+import pandas as pd 
+import numpy as np 
+# 读取数据
+import csv
+with open('F:\\watermelon.csv','r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+# 数据处理
+file = np.array(rows); file1 = file[1:,1:]#去除第一列第一行
+file2=pd.DataFrame(file1)# 去除重复数据
+file2.drop_duplicates(inplace=True)
+file3=np.array(file2); data = file3.astype(float)# 转换格式
+# 计算两向量的欧氏距离
+def distEclud(vecA, vecB):
+    return np.sqrt(np.sum(np.square(vecA-vecB)))
+
+# 循环
+def kMeans(data,matmean,dist=distEclud):
+    le=len(data)
+    cl = np.mat(np.zeros((le,2)))#创建结果分类矩阵
+    for q in range(999):
+        for i in range(le):
+            mindist=99999;index=-1
+        # 计算样本与均值向量的距离，确定样本簇标记
+            for j in range(2):
+                distji = dist(matmean[j],data[i])
+                if distji < mindist:
+                    mindist = distji;index = j
+            cl[i,:]=index,mindist
+    # 计算新均值向量
+        mattest = np.mat(np.zeros((2,2)))
+        for p in range(2):
+            clust = data[np.where(cl[:,0]==p)[0]]
+            mattest[p,:] = np.mean(clust,axis=0)#若将mattest初始化放于q的for循环外，此处第二次循环matmean随mattest变化
+        if (mattest == matmean).all() == True:#均值向量未改变
+            break
+        else:
+            matmean=mattest
+        #迭代第q次结果与q-1次相同
+    sumcl=np.sum(cl,axis=0)
+    sse=sumcl[0,1]
+    return [matmean,cl,sse,q-1]
+
+# 计算簇间距离
+def distbet(matmean, data):
+    allmean=np.mean(data,axis=0)
+    mean=np.tile(allmean,(2,1))
+    return np.sum(np.sqrt(np.sum(np.square(matmean-mean),axis=1)))/2#行相加开根号再相加
+# 计算簇内距离
+def distin(matmean, cl):
+    num0=np.sum(cl[:,0]==0)
+    num1=np.sum(cl[:,0]==1)
+    dist0=np.sum(cl[np.where(cl[:,0]==0)[0],1])
+    dist1=np.sum(cl[np.where(cl[:,0]==1)[0],1])
+    return dist0/num0 + dist1/num1
+
+le=np.shape(data)[0]
+# 随机选取一个初始化中心
+firmean=data[np.random.randint(le)]
+def secmean(data,firmean,dist=distEclud):
+    maxdist=0;index=-1
+    for i in range(le):
+    # 计算样本与均值向量的距离，选择距离最大的样本为第二个簇中心
+        disti = dist(firmean,data[i])
+        if disti > maxdist:
+            maxdist = disti
+            index=i
+    return data[index]
+fsmean=np.vstack((firmean,secmean(data,firmean))) 
+
+#性能评价
+ass=[]
+for i in range(le):
+    firmean=data[i]
+    fsmean=np.vstack((firmean,secmean(data,firmean)))
+    a=kMeans(data,fsmean)
+    matmean=a[0]
+    cl=a[1]
+    x=distbet(matmean,data)
+    y=distin(matmean,cl)
+    disass=distbet(matmean,data)/distin(matmean,cl)
+    ass.append(disass)#评价指标形成列表
+print(max(ass),min(ass))
+
+# 指标散点图
+x=range(len(ass))
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']#图片显示中文，参考网址：https://blog.csdn.net/seveneleve/article/details/108974201
+p1=plt.scatter(x,ass,marker='.')
+plt.title('K-means++算法性能评估指数散点图')
+plt.xlabel("编号")
+plt.ylabel("指数")
+plt.show()
+
+
